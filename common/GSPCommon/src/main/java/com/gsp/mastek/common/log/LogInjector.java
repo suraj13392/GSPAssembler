1877464617ae3f605b1ea6114d56e200b54c2337
shivkumarg <shivgonarkar@gmail.com>
2017-04-24 14:16:15 +0530

    Added code for logging framework

---
+++ common/GSPCommon/src/main/java/com/gsp/mastek/common/log/LogInjector.java
@@ -0,0 +1,30 @@
+package com.gsp.mastek.common.log;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+import java.lang.reflect.Field;
+
+@Component
+public class LogInjector implements BeanPostProcessor {
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    public Object postProcessBeforeInitialization(final Object bean, String name) throws BeansException {
+        ReflectionUtils.doWithFields(bean.getClass(), new ReflectionUtils.FieldCallback() {
+            public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+                // make the field accessible if defined private
+                ReflectionUtils.makeAccessible(field);
+                if (field.getAnnotation(Log.class) != null) {
+                    Logger log = Logger.getLogger(bean.getClass());
+                    field.set(bean, log);
+                }
+            }
+        });
+        return bean;
+    }
+}
