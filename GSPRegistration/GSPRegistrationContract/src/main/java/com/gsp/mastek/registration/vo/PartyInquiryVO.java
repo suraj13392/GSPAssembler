66179b320785b84b86806d75f56e99ddec8da4fa
suraj13392 <suraj13392@IND-MHP1DW70460.mastek.com>
2017-04-27 15:27:07 +0530

    PartyInquiryVO added for fetching party related data

---
+++ GSPRegistration/GSPRegistrationContract/src/main/java/com/gsp/mastek/registration/vo/PartyInquiryVO.java
@@ -0,0 +1,65 @@
+package com.gsp.mastek.registration.vo;
+
+import com.gsp.mastek.common.vo.BaseVO;
+
+public class PartyInquiryVO extends BaseVO {
+	
+	private Long organizationId;
+	private String gstin;
+	private Long partyId;
+	private String aadharNumber;
+	private String panNumber;
+	private String userName;
+	private String partyStatus;
+	
+	public Long getOrganizationId() {
+		return organizationId;
+	}
+	public void setOrganizationId(Long organizationId) {
+		this.organizationId = organizationId;
+	}
+	public String getGstin() {
+		return gstin;
+	}
+	public void setGstin(String gstin) {
+		this.gstin = gstin;
+	}
+	public Long getPartyId() {
+		return partyId;
+	}
+	public void setPartyId(Long partyId) {
+		this.partyId = partyId;
+	}
+	public String getAadharNumber() {
+		return aadharNumber;
+	}
+	public void setAadharNumber(String aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+	public String getPanNumber() {
+		return panNumber;
+	}
+	public void setPanNumber(String panNumber) {
+		this.panNumber = panNumber;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPartyStatus() {
+		return partyStatus;
+	}
+	public void setPartyStatus(String partyStatus) {
+		this.partyStatus = partyStatus;
+	}
+	@Override
+	public String toString() {
+		return "PartyInquiryVO [organizationId=" + organizationId + ", gstin=" + gstin + ", partyId=" + partyId
+				+ ", aadharNumber=" + aadharNumber + ", panNumber=" + panNumber + ", userName=" + userName
+				+ ", partyStatus=" + partyStatus + "]";
+	}
+	
+	
+}
