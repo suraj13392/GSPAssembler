e99252c358fd7e53d3a12c1c3f54b6164bb0b50a
priyank13476 <priyank13476@IND-MHP1DW78340.mastek.com>
2017-05-05 12:34:41 +0530

    GSPUI Changes Added

--- GSPUI/src/main/java/com/gsp/mastek/GspUIApplication.java
+++ GSPUI/src/main/java/com/gsp/mastek/GspUIApplication.java
@@ -2,9 +2,8 @@ package com.gsp.mastek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
+
 @SpringBootApplication
 public class GspUIApplication {
 
